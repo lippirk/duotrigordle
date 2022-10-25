7ddb3ca03bf5a4910d1b22b8bb04ec6c1474d637
Ben Anson <24416311+lippirk@users.noreply.github.com>
2022-10-25 20:42:08 +0100

    gen: add italian

--- gen/jsonify_words.sh
+++ gen/jsonify_words.sh
@@ -9,3 +9,9 @@ jq --raw-input . < ./valid_fr.txt| jq --slurp . > ./valid_fr.json
 
 # target words
 cp ../../wordle-fr/src/assets/json/drawable-words.json ./target_fr.json
+
+### from wordle-it
+cp ../../wordle-it/dict/parole.txt ./target_it.txt
+
+# convert to json
+jq --raw-input . < ./target_it.txt| jq --slurp . > ./target_it.json
